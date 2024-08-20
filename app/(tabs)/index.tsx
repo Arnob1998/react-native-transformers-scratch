ab80b3286882c950cbd33734ae1ef4bfc54ac6f9
Redwan Hossain Arnob <46885415+Arnob1998@users.noreply.github.com>
2024-08-20 19:12:13 +0600

    downloadable model

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,6 +8,8 @@ import {
   Button,
 } from "react-native";
 
+import { useEffect } from "react";
+
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
@@ -17,7 +19,8 @@ import * as ort from "onnxruntime-react-native";
 import { Asset, useAssets } from "expo-asset";
 import { tokenizer } from '@/constants/tokenizer';
 import * as FileSystem from 'expo-file-system';
-import { env, AutoTokenizer, PreTrainedTokenizer } from "@xenova/transformers";
+import { env, AutoTokenizer, PreTrainedTokenizer, pipeline, AutoModel } from "@xenova/transformers";
+import { InferenceSession } from 'onnxruntime-react-native';
 
 export default function HomeScreen() {
   console.log("At home");
@@ -139,7 +142,6 @@ export default function HomeScreen() {
       // Download the file
       const response = await FileSystem.downloadAsync(url, fileUri);
       
-      console.log('File downloaded to:', response.uri);
       return response.uri; // Return the URI for further use
     } catch (error) {
       console.error('Error downloading file:', error);
@@ -195,6 +197,145 @@ export default function HomeScreen() {
     }
   };
 
+
+  const loadSmolLM360MInstructTransformerPipeline = async () => {
+    try {
+      const tokenizer_url = 'https://raw.githubusercontent.com/Arnob1998/react-native-transformers-scratch/main/assets/SmolLM-360M-Instruct/tokenizer.json';
+      const tokenizer_filename = 'tokenizer.json';
+      const tokenizer_config_filename = 'tokenizer_config.json';
+      const tokenizer_config_url = "https://raw.githubusercontent.com/Arnob1998/react-native-transformers-scratch/main/assets/SmolLM-360M-Instruct/tokenizer_config.json"
+      const folderName = 'SmolLM-360M-Instruct';
+
+      const tokenizerfileUri = await saveFileToFolder(tokenizer_url, folderName, tokenizer_filename);
+      const tokenizer_configUri = await saveFileToFolder(tokenizer_config_url, folderName, tokenizer_config_filename);
+  
+      if (tokenizerfileUri && tokenizer_configUri) {
+        // You can now use the fileUri to read or process the file later
+        console.log('File is ready to use:', tokenizerfileUri, tokenizer_configUri);
+
+        env.localModelPath = FileSystem.documentDirectory;
+        env.allowRemoteModels = false; 
+        const messages = [{"role": "user", "content": "What is the capital of France."}]
+        console.log("Local model path set to:", env.localModelPath);
+        const tokenizer = await AutoTokenizer.from_pretrained(folderName);
+        const tokens = tokenizer.apply_chat_template(messages, tokenize=false)
+        console.log("tokens", tokens);
+
+              // Convert tokens to ONNX tensors
+      const inputIdsTensor = new ort.Tensor('int64', tokens.data, tokens.dims);
+      const attentionMask = new Array(tokens.dims[1]).fill(1); // Assuming attention mas
+      // Generate position_ids (0, 1, 2, ..., n-1)
+      const positionIds = Array.from({ length: tokens.dims[1] }, (_, i) => i);
+      const positionIdsTensor = new ort.Tensor('int64', positionIds, [1, tokens.dims[1]]);
+
+      console.log("inputIdsTensor", inputIdsTensor);
+      console.log("attentionMask", attentionMask);
+
+
+      const feeds = {
+        input_ids: inputIdsTensor,
+        attention_mask: new ort.Tensor('int64', attentionMask, [1, tokens.dims[1]]),
+        position_ids: positionIdsTensor
+      };
+      
+      console.log("loading the model")
+      // Load ONNX model
+      const assets = await Asset.loadAsync(require("@/assets/SmolLM-360M-Instruct/model_quantized.onnx"));
+      const modelUri = assets[0].localUri;
+
+      if (!modelUri) {
+        console.log("Failed to get model URI", `${assets[0]}`);
+        return;
+      }
+
+      const myModel = await ort.InferenceSession.create(modelUri);
+      console.log("Model loaded successfully. Input names:", myModel.inputNames, "Output names:", myModel.outputNames);
+
+      // Run inference
+      const results = await myModel.run(feeds);
+      console.log("Inference results:", results);
+
+        // console.log("loading model");
+        // const assets = await Asset.loadAsync(require("@/assets/SmolLM-360M-Instruct/model_quantized.onnx"));
+        // const modelUri = assets[0].localUri;
+        // console.log("modelUri ", modelUri);
+        // if (!modelUri) {
+        //   console.log("failed to get model URI", `${assets[0]}`);
+        // } 
+        //   // load model from model url path
+        //   myModel = await ort.InferenceSession.create(modelUri);
+        //   console.log(
+        //     "model loaded successfully",
+        //     `input names: ${myModel.inputNames}, output names: ${myModel.outputNames}`)
+        // // Run inference
+        // const results = await myModel.run(tokens);
+        // console.log(results);
+      }
+    } catch (e) {
+      Alert.alert("failed to inference model in loadTransformerPipelineToken", `${e}`);
+      console.log(e)
+      throw e;
+    }
+  };
+
+  const autoSmolLM360MInstructPipeline = async () => {
+    try {
+      const config_url = 'https://drive.google.com/uc?export=download&id=1-6Eh3FMeMlBycs09y59pQtAR0ffmEUtp';
+      const config_filename = 'config.json';
+      const tokenizer_url = 'https://drive.google.com/uc?export=download&id=1-N-3s4qtM45lk33y8Oc6KTV6VnPCB2YW';
+      const tokenizer_filename = 'tokenizer.json';
+      const tokenizer_config_filename = 'tokenizer_config.json';
+      const tokenizer_config_url = "https://drive.google.com/uc?export=download&id=1-F9jFH_uWPuaoUhFJY2HxiNvDxzK2kE_"
+      const model_quantized_filename = 'decoder_model_merged_quantized.onnx';
+      const model_quantized_config_url = "https://drive.google.com/file/d/1-D9hC4XNnI8yHwRfg1L7qp8KXZH7BF5e/view?usp=sharing"
+      const folderName = 'SmolLM-360M-Instruct';
+      const model_quantizedfolderName = 'SmolLM-360M-Instruct/onnx/';
+
+      const configfileUri = await saveFileToFolder(config_url, folderName, config_filename);
+      console.log("configfileUri downloaded")
+      const tokenizerfileUri = await saveFileToFolder(tokenizer_url, folderName, tokenizer_filename);
+      console.log("tokenizerfileUri downloaded")
+      const tokenizer_configUri = await saveFileToFolder(tokenizer_config_url, folderName, tokenizer_config_filename);
+      console.log("tokenizer_configUri downloaded")
+      const model_quantizedUri = await saveFileToFolder(model_quantized_config_url, model_quantizedfolderName, model_quantized_filename);
+      console.log("model_quantizedUri downloaded")
+
+      if (tokenizerfileUri && tokenizer_configUri && model_quantizedUri && configfileUri) {
+        // You can now use the fileUri to read or process the file later
+        console.log('Files is ready to use');
+
+        env.localModelPath = FileSystem.documentDirectory;
+        env.allowRemoteModels = false; 
+        console.log("Local model path set to:", env.localModelPath);
+
+        
+        const model = await AutoModel.from_pretrained(folderName, {
+          backend: 'onnx',
+          // Pass the session creation logic if required
+          sessionOptions: {
+            executionProviders: ['cpu'], // Example, use 'cpu' or 'cuda' depending on your setup
+          },
+          onnx: {
+            session: new InferenceSession({ backend: 'cpu' }),
+          },
+        });
+        const tokenizer = await AutoTokenizer.from_pretrained(folderName);
+
+        console.log(model, tokenizer)
+
+
+      }
+
+
+    } catch (e) {
+      Alert.alert("failed to inference model in loadTransformerPipelineToken", `${e}`);
+      console.log(e)
+      throw e;
+    }
+  };
+
+
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -221,7 +362,9 @@ export default function HomeScreen() {
       <Button title="loadDistilModel" onPress={loadDistilModel}/> 
       <Button title="loadDistilTokenizer" onPress={loadDistilTokenizer}/> 
       <Button title="loadDistilPipelineModel" onPress={loadDistilPipelineModel}/> 
-      <Button title="loadTransformerPipelineToken" onPress={loadTransformerPipelineToken}/> 
+      <Button title="loadTransformerPipelineToken" onPress={loadTransformerPipelineToken}/>  
+      <Button title="loadSmolLM360MInstructTransformerPipeline" onPress={loadSmolLM360MInstructTransformerPipeline}/>
+      <Button title="autoSmolLM360MInstructPipeline" onPress={autoSmolLM360MInstructPipeline}/>
       {/* <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
